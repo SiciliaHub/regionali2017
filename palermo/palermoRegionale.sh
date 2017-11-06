a28065e7f4e17b8d19e335dd3e5959c43aa97fb9
aborruso <aborruso@gmail.com>
2017-11-06 23:53:24 +0100

    update

--- palermo/palermoRegionale.sh
+++ palermo/palermoRegionale.sh
@@ -13,7 +13,7 @@ sed -i '1s/^/idLista,nomeLista\n/' ./dati/listeProvinciaPa.csv
 paragrafo=6
 for i in {1..12}
 do
-    <./dati/votiCandidatiProvinciaPa.html pup "body > div:nth-child(1) > table:nth-child(2)  > tbody > tr:nth-child(2) > td:nth-child(3) > table > tbody > tr:nth-child(2) > td > p:nth-child($paragrafo)" | scrape -be "//table//tr[position() > 2 and position() < last()]" | sed 's/<br\/>/ /g ; s/  / /g' | xml2json | sed 's/\\n   \\n   / /g' | jq '[.html.body.tr[] | {lista:"'"$i"'",numero:.td[0]."$t",nome:.td[1]."$t",anagrafica:.td[2]."$t",preferenze:.td[3]."$t"|gsub("\\."; "")}]' | in2csv --no-inference -f json > ./dati/L_R"$i".csv
+    <./dati/votiCandidatiProvinciaPa.html pup "body > div:nth-child(1) > table:nth-child(2)  > tbody > tr:nth-child(2) > td:nth-child(3) > table > tbody > tr:nth-child(2) > td > p:nth-child($paragrafo)" | scrape -be "//table//tr[position() > 2 and position() < last()]" | sed 's/<br\/>/ /g ; s/  / /g' | xml2json | sed 's/\\n   \\n   / /g' | jq '[.html.body.tr[] | {lista:"'"$i"'",numeroCandidato:.td[0]."$t",nome:.td[1]."$t",anagrafica:.td[2]."$t",preferenze:.td[3]."$t"|gsub("\\."; "")}]' | in2csv --no-inference -f json > ./dati/L_R"$i".csv
     paragrafo=$(expr $paragrafo + 4)
 done
 
